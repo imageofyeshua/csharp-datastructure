e3ccd0d0ec1b9c4e1e8adabb04b78dd38b371b62
Daniel Park <intheimageofyeshua@gmail.com>
2024-11-21 10:06:05 +0900

    Enum

--- DataType/Program.cs
+++ DataType/Program.cs
@@ -36,5 +36,55 @@
         System.Console.WriteLine($"Char: {bullet}");
         System.Console.WriteLine($"Char: {special}");
 
+        const int DaysInWeek = 7;
+        System.Console.WriteLine($"Const: {DaysInWeek}");
+
+        CurrencyEnum currency = CurrencyEnum.WON;
+
+        switch(currency)
+        {
+            case CurrencyEnum.WON:
+                System.Console.WriteLine("It's Won");
+                break;
+            case CurrencyEnum.EUR:
+                System.Console.WriteLine("It's Euro");
+                break;
+            case CurrencyEnum.USD:
+                System.Console.WriteLine("It's Dollar");
+                break;
+            default:
+                System.Console.WriteLine("Nothing");
+                break;
+        }
+
+        ActionEnum guest = ActionEnum.List;
+        System.Console.WriteLine($"Guest: {guest.ToString("G")}");
+        System.Console.WriteLine($"Guest: {guest.ToString("F")}");
+        System.Console.WriteLine($"Guest: {guest.ToString("D")}");
+        System.Console.WriteLine($"Guest: {guest.ToString("X")}");
+
+        ActionEnum user = ActionEnum.List | ActionEnum.Details;
+        System.Console.WriteLine($"User: {user.ToString("X")}");
+        ActionEnum editor = ActionEnum.List | ActionEnum.Add | ActionEnum.Edit;
+        System.Console.WriteLine($"Editor: {editor.ToString("X")}");
+    }
+
+    [Flags]
+    enum ActionEnum
+    {
+        None = 0b_0000_0000, // 0
+        List = 0b_0000_0001, // 1
+        Details = 0b_0000_0010, // 2
+        Add = 0b_0000_0100, // 4
+        Edit = 0b_0000_1000, // 8
+        Delete = 0b_0001_0000, // 16
+        Publicsh = 0b_0010_0000, // 32
+    }
+
+    enum CurrencyEnum
+    {
+        WON,
+        USD,
+        EUR
     }
 }
