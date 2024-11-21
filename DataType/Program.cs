9ed62e842dc5786521c85654d305c21675febef7
Daniel Park <intheimageofyeshua@gmail.com>
2024-11-21 09:46:51 +0900

    Initial Commit

---
+++ DataType/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int a = -20;
+        byte b = 0x0f;
+        uint c = 0b01101110;
+
+        System.Console.WriteLine($"Integer: {a}");
+        System.Console.WriteLine($"Byte: {b}");
+        System.Console.WriteLine($"Unsigned Integer: {c}");
+
+        float temperature = 36.6f;
+        double reading = -4.5179823;
+        decimal salary = 100000.47M;
+
+        System.Console.WriteLine($"Float: {temperature}");
+        System.Console.WriteLine($"Double: {reading}");
+        System.Console.WriteLine($"Decimal: {salary}");
+
+        bool isTrue = true;
+        bool first = isTrue || false;
+        bool second = isTrue && false;
+        bool third = 50 > 10;
+
+        System.Console.WriteLine($"Bool: {isTrue}");
+        System.Console.WriteLine($"Bool: {first}");
+        System.Console.WriteLine($"Bool: {second}");
+        System.Console.WriteLine($"Bool: {third}");
+
+        char letter = 'M'; // character literal
+        char bullet = '\u25cf'; // unicode
+        char special = '\x107'; // hexadecimal
+
+        System.Console.WriteLine($"Char: {letter}");
+        System.Console.WriteLine($"Char: {bullet}");
+        System.Console.WriteLine($"Char: {special}");
+
+    }
+}
