2cc600b741aa80d5eb64fcf08a8a0c134f16930f
Daniel Park <intheimageofyeshua@gmail.com>
2024-11-22 14:13:16 +0900

    User Defined Struct

---
+++ UserDefinedStruct/Program.cs
@@ -0,0 +1,18 @@
+﻿public enum CurrencyEnum { WON, EUR, USD }
+
+public readonly struct Price(decimal amount, CurrencyEnum currency)
+{
+    public readonly decimal Amount { get; init; } = amount;
+    public readonly CurrencyEnum Currency { get; init; } = currency;
+
+    public override string ToString() => $"{Amount} {Currency}";
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Price priceRegular = new(100, CurrencyEnum.WON);
+        System.Console.WriteLine(priceRegular);
+    }
+}
